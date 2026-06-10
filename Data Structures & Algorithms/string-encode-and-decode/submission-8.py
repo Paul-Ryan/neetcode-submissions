d589626e8bd3acddbf681197c214a5963891b39d
Paul Ryan <paul.ryan.iii@gmail.com>
2026-06-09 19:36:53 -0700

    Add: string-encode-and-decode - submission-8

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-8.py
@@ -0,0 +1,23 @@
+class Solution:
+    # first combine the strings with a length and special delimiter
+    # decode the strings by reading the length, then reading that number of characters
+    # add the characters you read into an array as a string and repeat
+    def encode(self, strs: list[str]) -> str:
+        return "".join(f"{len(s)}#{s}" for s in strs)
+
+    def decode(self, s: str) -> list[str]:
+        res = []
+        i = 0
+        length = ""
+        while i < len(s):
+            char = s[i]
+            if char.isdigit():
+               length = length + char
+            if char == "#":
+                wl = int(length)
+                res.append(s[i + 1:i + wl + 1]) 
+                i = i + wl
+                length = ""
+            i += 1    
+                    
+        return res
